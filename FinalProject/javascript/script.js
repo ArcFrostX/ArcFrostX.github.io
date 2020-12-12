d429a7426cea901b45fc55d6d320616593f9af1f
ArcFrostX <52903469+ArcFrostX@users.noreply.github.com>
2020-12-11 22:23:06 -0500

    Update script.js

--- FinalProject/javascript/script.js
+++ FinalProject/javascript/script.js
@@ -1,10 +1,10 @@
 function initMap(){
-    const location = {lat: 42.70,lng: -73.10};
-    const map = new google.maps.Map(document.getElementById("map"), {
+    var location = {lat: 42.70,lng: -73.10};
+    var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 4,
         center: location,
     });
-    const marker = new google.maps.Marker({
+    var marker = new google.maps.Marker({
         position: location,
         map: map,
     });

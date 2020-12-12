00a19896fc1f462a2af2aeb2985663437f617535
ArcFrostX <52903469+ArcFrostX@users.noreply.github.com>
2020-12-11 21:57:41 -0500

    WebDevFinal

---
+++ FinalProject/javascript/script.js
@@ -0,0 +1,11 @@
+function initMap(){
+    const location = {lat: 42.70,lng: -73.10};
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: location,
+    });
+    const marker = new google.maps.marker({
+        position: location,
+        map: map,
+    });
+}
